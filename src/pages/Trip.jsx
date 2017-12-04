01ae44638ed2f698cd1a98b9bcb736962a1da536
Anastasia <anastisiamazur96@gmail.com>
2017-12-04 11:57:05 +0200

    add back button to requirement-4

--- src/pages/Trip.jsx
+++ src/pages/Trip.jsx
@@ -5,7 +5,13 @@ export default class Trip extends Component {
     render() {
         return <Layout>
             <div style={{height: '70vh'}} className='d-flex justify-content-center align-items-center'>
-                <div className='dummy-image'>dummy page</div>
+                <div>
+                    <div className='dummy-image text-center'>dummy page</div>
+                    <br/>
+                    <button className='btn btn-primary' onClick={this.props.history.goBack}>
+                        Go Back
+                    </button>
+                </div>
             </div>
         </Layout>
     }
