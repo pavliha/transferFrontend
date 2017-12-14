aadaf004b3b4257cddbdc4dff28ee5238d272a38
Anastasia <anastisiamazur96@gmail.com>
2017-12-14 09:12:39 +0200

    fix deployment issues

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/api/expenses', (req, res) => {
         ]
     })
 })
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
