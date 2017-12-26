712f698d71250f14aa9844086fbf40acc5318806
Aleksey <lukerlexa@gmail.com>
2017-12-26 22:07:51 +0200

    Сделал стартовую страничку регистрации

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import Route from "react-router/es/Route";
 import {Switch} from "react-router-dom";
 import Index from './pages/Index'
 import Login from './pages/Login'
+import Registration from "./pages/Registration";
 import {createBrowserHistory} from 'history'
 import {Router} from "react-router";
 
@@ -12,6 +13,7 @@ export default () =>
             <Switch>
                 <Route exact path="/" component={Index}/>
                 <Route exact path="/login" component={Login}/>
+                <Route exact path="/registration" component={Registration}/>
             </Switch>
         </Router>
 
