f925d771ff97fce195341c75ca541e84dcd4c447
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2018-06-12 22:46:53 +0300

    update FilterCard

--- src/components/@cargo/FilterCard/index.jsx
+++ src/components/@cargo/FilterCard/index.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Card from '@material-ui/core/es/Card/Card'
 import CardContent from '@material-ui/core/es/CardContent/CardContent'
 import CardActions from '@material-ui/core/es/CardActions/CardActions'
 import Button from '@material-ui/core/es/Button/Button'
 import AdditionalFilters from './AdditionalFilters'
 import Filters from './Filters'
+import connector from './connector'
 
-const FilterCard = () =>
+const FilterCard = ({ selected }) =>
   <Card>
     <CardContent>
       <Filters />
       <AdditionalFilters />
     </CardContent>
+    {!!selected.length &&
     <CardActions>
       <Button fullWidth variant="raised" size="large" color="primary">
         Искать
       </Button>
     </CardActions>
+    }
   </Card>
 
-FilterCard.propTypes = {}
+FilterCard.propTypes = {
+  selected: PropTypes.array.isRequired,
+}
 
-export default FilterCard
+export default connector(FilterCard)
